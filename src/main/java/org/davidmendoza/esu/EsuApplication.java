b09c1e692120f8516b5299b09cd9ac4c46532cd4
J. David Mendoza <jdmendoza@swau.edu>
2015-03-11 05:56:37 -0500

    ESU

---
+++ src/main/java/org/davidmendoza/esu/EsuApplication.java
@@ -0,0 +1,12 @@
+package org.davidmendoza.esu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsuApplication.class, args);
+    }
+}
