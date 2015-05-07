66a79586fa6aaf23c53145c9bc0fef53db9f8222
J. David Mendoza <jdmendozar@gmail.com>
2015-05-07 09:51:11 -0500

    Muestra los versículos de la biblia

---
+++ src/main/java/org/davidmendoza/esu/service/impl/BibliaServiceImpl.java
@@ -0,0 +1,52 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 J. David Mendoza.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.davidmendoza.esu.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.davidmendoza.esu.dao.BibliaDao;
+import org.davidmendoza.esu.model.Biblia;
+import org.davidmendoza.esu.model.Rv2000;
+import org.davidmendoza.esu.service.BaseService;
+import org.davidmendoza.esu.service.BibliaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author J. David Mendoza <jdmendoza@swau.edu>
+ */
+@Service
+@Transactional
+public class BibliaServiceImpl extends BaseService implements BibliaService {
+    
+    @Autowired
+    private BibliaDao bibliaDao;
+
+    @Override
+    public List<Rv2000> biblia(Integer libro, Integer capitulo, Integer versiculo, Integer versiculos) {
+        return bibliaDao.biblia(libro, capitulo, versiculo, versiculos);
+    }
+}
