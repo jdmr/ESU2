a15b3e9c86f66d6e4f83b5330fdafd23412ab5ac
J. David Mendoza <jdmendoza@swau.edu>
2015-11-25 05:49:56 -0600

    Configuración del Tomcat AJP

---
+++ src/main/java/org/davidmendoza/esu/config/TomcatConfig.java
@@ -0,0 +1,72 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 J. David Mendoza.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.davidmendoza.esu.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.valves.RemoteIpValve;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author J. David Mendoza <jdmendoza@swau.edu>
+ */
+@Configuration
+public class TomcatConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(TomcatConfig.class);
+    
+    @Value("${server.use-forward-headers}")
+    private Boolean forwardHeaders = Boolean.TRUE;
+
+    @Bean
+    @SuppressWarnings("static-method")
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addAdditionalTomcatConnectors(createConnector());
+        if (forwardHeaders) {
+            log.info("Forwarding headers");
+            tomcat.addContextValves(createRemoteIpValves());
+        }
+        return tomcat;
+    }
+
+    private static RemoteIpValve createRemoteIpValves() {
+        RemoteIpValve remoteIpValve = new RemoteIpValve();
+        remoteIpValve.setRemoteIpHeader("x-forwarded-for");
+        remoteIpValve.setProtocolHeader("x-forwarded-proto");
+        return remoteIpValve;
+    }
+
+    private static Connector createConnector() {
+        Connector connector = new Connector("AJP/1.3");
+        connector.setPort(8009);
+        return connector;
+    }
+}
